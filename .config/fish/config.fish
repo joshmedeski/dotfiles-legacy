a949e7cd2299e7ab6909f5490512bb22151757bf
Josh Medeski <josh.medeski@gmail.com>
2020-10-19 11:00:16 -0500

    fzf: remove background color on rows

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -9,7 +9,7 @@ set -x PATH (pwd)"/git-fuzzy/bin:$PATH"
 # Setting fd as the default source for fzf
 export FZF_DEFAULT_COMMAND='fd --type f --hidden --follow --exclude .git'
 export FZF_DEFAULT_OPTS='
-    --color=fg:#e5e9f0,bg:#2e3440,hl:#81a1c1
+    --color=fg:#e5e9f0,hl:#81a1c1
     --color=fg+:#e5e9f0,bg+:#3b4252,hl+:#81a1c1
     --color=info:#eacb8a,prompt:#bf6069,pointer:#b48dac
     --color=marker:#a3be8b,spinner:#b48dac,header:#a3be8b
