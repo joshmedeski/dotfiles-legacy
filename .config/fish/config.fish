1111aede2e20592bf6fac3a8420438f08c5b6bbd
Josh Medeski <josh.medeski@gmail.com>
2020-10-18 22:02:39 -0500

    fish: update config

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -8,13 +8,15 @@ set -x PATH (pwd)"/git-fuzzy/bin:$PATH"
 
 # Setting fd as the default source for fzf
 export FZF_DEFAULT_COMMAND='fd --type f --hidden --follow --exclude .git'
-export FZF_DEFAULT_OPTS='--height 80% --reverse'
+export FZF_DEFAULT_OPTS='
+    --color=fg:#e5e9f0,bg:#2e3440,hl:#81a1c1
+    --color=fg+:#e5e9f0,bg+:#3b4252,hl+:#81a1c1
+    --color=info:#eacb8a,prompt:#bf6069,pointer:#b48dac
+    --color=marker:#a3be8b,spinner:#b48dac,header:#a3be8b
+    --height 80% --reverse'
 export GOPATH=(go env GOPATH)
 
 set -Ux EDITOR nvim
-
-alias l="exa -a --group-directories-first"
-alias ll="exa -al --group-directories-first"
 
 alias g="lazygit"
 alias ga="git add"
@@ -26,19 +28,17 @@ alias gl="git pull"
 alias gp="git push"
 alias gpr="gh pr create"
 alias gs="git status"
-
+alias l="exa -a --group-directories-first"
+alias ll="exa -al --group-directories-first"
+alias tmuxx="tmux attach-session -t (tmux list-sessions | fzf | cut -d ':' -f 1)"
+alias tnl="nvim ~/.todoist/labels/(todoist labels | fzf | cut -d ' ' -f 1 | tr -d '[:space:]').md"
+alias tnp="nvim ~/.todoist/projects/(todoist projects | fzf | cut -d ' ' -f 1 | tr -d '[:space:]').md"
+alias tnt="nvim ~/.todoist/tasks/(todoist show | fzf | cut -d ' ' -f 1 | tr -d '[:space:]').md"
+alias tx=tmuxinator
 alias u="ultralist"
 alias uc="ultralist c (ul | fzf | cut -c 1)"
 alias ul="ultralist list not:completed group:context"
 alias up="~/bin/update.sh"
-
-alias tnp="nvim ~/.todoist/projects/(todoist projects | fzf | cut -d ' ' -f 1 | tr -d '[:space:]').md"
-alias tnl="nvim ~/.todoist/labels/(todoist labels | fzf | cut -d ' ' -f 1 | tr -d '[:space:]').md"
-alias tnt="nvim ~/.todoist/tasks/(todoist show | fzf | cut -d ' ' -f 1 | tr -d '[:space:]').md"
-
-alias tmuxx="tmux attach-session -t (tmux list-sessions | fzf | cut -d ':' -f 1)"
-alias tx=tmuxinator
-
 alias v="v (fzf)"
 
 set -g fish_user_paths "/usr/local/sbin" $fish_user_paths
