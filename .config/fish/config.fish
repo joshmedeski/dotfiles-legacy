3c1673d3af896539bc7497da977b3556adc022e1
Josh Medeski <josh.medeski@gmail.com>
2020-10-19 11:00:30 -0500

    fzf: make height shorter

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -13,7 +13,7 @@ export FZF_DEFAULT_OPTS='
     --color=fg+:#e5e9f0,bg+:#3b4252,hl+:#81a1c1
     --color=info:#eacb8a,prompt:#bf6069,pointer:#b48dac
     --color=marker:#a3be8b,spinner:#b48dac,header:#a3be8b
-    --height 80% --reverse'
+    --height 50% --reverse'
 export GOPATH=(go env GOPATH)
 
 set -Ux EDITOR nvim
