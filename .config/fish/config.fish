3add094bc271959db15882f7555a792dc4cd2892
Josh Medeski <josh.medeski@gmail.com>
2020-11-01 00:48:42 -0500

    [fish] Use more simple nord theme for fzf

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -9,11 +9,9 @@ set -x PATH (pwd)"/git-fuzzy/bin:$PATH"
 # Setting fd as the default source for fzf
 export FZF_DEFAULT_COMMAND='fd --type f --hidden --follow --exclude .git'
 export FZF_DEFAULT_OPTS='
-    --color=fg:#e5e9f0,hl:#81a1c1
-    --color=fg+:#e5e9f0,bg+:#3b4252,hl+:#81a1c1
-    --color=info:#eacb8a,prompt:#bf6069,pointer:#b48dac
-    --color=marker:#a3be8b,spinner:#b48dac,header:#a3be8b
-    --height 50% --reverse'
+    --color fg:#D8DEE9,bg:#2E3440,hl:#A3BE8C,fg+:#D8DEE9,bg+:#434C5E,hl+:#A3BE8C
+    --color pointer:#BF616A,info:#4C566A,spinner:#4C566A,header:#4C566A,prompt:#81A1C1,marker:#EBCB8B
+    --reverse'
 export GOPATH=(go env GOPATH)
 
 set -Ux EDITOR nvim
